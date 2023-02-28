48d26f248a13ec72fce3ff01215e285891b13a55
wasssiliyy <wasssiliyy1@gmail.com>
2023-02-28 22:38:31 +0400

    Add project files.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (UIElement el in GroupButton.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += ButtonClick;
+                }
+            }
+        }
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+
+            string textButton = ((Button)e.OriginalSource).Content.ToString();
+
+            if (textButton == "C")
+            {
+                text.Text = text.Text.Substring(text.Text.Length);
+            }
+            else if (textButton == "=")
+            {
+                text.Text = new DataTable().Compute(text.Text, null).ToString();
+            }
+            else text.Text += textButton;
+        }
+
+
+    }
+}
